3590d516d6e02d80cc6f7575a58e05c65b8ebdb1
Iam Slaughter <jlgslaughter@gmail.com>
2018-05-29 12:14:16 -0400

    my first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const fill = document.querySelector('.fill');
+const empties = document.querySelectorAll('.empty');
+
+//Fill listeners
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
+
+//drag functions
+function dragStart() {
+    this.className += ' hold';
+    setTimeout(() => { 
+        this.className = 'invisible'}, 0)
+}
+
+function dragEnd() {
+    this.className = 'fill';
+}
